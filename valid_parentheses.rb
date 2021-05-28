d5a498f2e84a5aca2e5fae9d1d59a2aa702bb122
Sofia <jonssonsofia93@gmail.com>
2021-05-27 21:08:07 -0600

    valid parentheses ruby solution

---
+++ valid_parentheses.rb
@@ -0,0 +1,18 @@
+def is_valid(s)
+     return true if s.empty?
+
+    stack = []
+    s.each_char do |c|
+        case c
+        when '(', '{', '['
+            stack.push(c)
+        when ')'
+            return false if stack.pop() != '('
+        when '}'
+            return false if stack.pop() != '{'
+        when ']'
+            return false if stack.pop() != '['
+        end
+    end
+    return stack.empty?
+end

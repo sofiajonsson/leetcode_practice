3e99ae973abbd8e5e722cb7167f78d84827550dd
Sofia <jonssonsofia93@gmail.com>
2021-06-21 05:19:52 -0600

    reverse string ruby solution

--- reverse_string.rb
+++ reverse_string.rb
@@ -1,3 +1,17 @@
 def reverse_string(s)
     s.reverse!
 end
+
+def reverse_string(s)
+
+  return true if s.size == 0
+  left = 0
+  right = s.size - 1
+
+  while(left <= right) do
+    s[left], s[right] = s[right], s[left]
+    left += 1
+    right -= 1
+  end
+
+end
